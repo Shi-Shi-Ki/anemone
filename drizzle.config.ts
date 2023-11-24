a687811a449571f3063b83b4f8879d39a7921f58
Shi-Shi-Ki <tac829@gmail.com>
2023-11-24 16:41:54 +0900

    first commit with drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  schema: './db/schema.ts',
+  out: './db',
+  driver: 'mysql2',
+  dbCredentials: {
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.MYSQL_DATABASE,
+  },
+} satisfies Config;
